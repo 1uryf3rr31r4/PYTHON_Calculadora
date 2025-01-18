a3f4a973b6c1c85d51506e125d118ddaaa81ad89
Iury Ferreira <1uryf3rr31r4@gmail.com>
2025-01-18 10:08:00 -0300

    Função Menu Principal

--- PYTHON_Calculadora.py
+++ PYTHON_Calculadora.py
@@ -1,6 +1,11 @@
 import math
 from logging import exception
 
+def menu_principal():
+    print('Calculadora'.center(65,'='))
+    opcoes()
+    print('='*65)
+
 def dividir(num1, num2):
     try:
         return f'{num1/num2}={num1/num2}'
